103c3d25d7acd06d15617425815020da18ba754a
nathangrieve83 <Nate@10.211.55.3>
2019-04-13 13:55:50 +1000

    reverted text

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user again
+		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
 
